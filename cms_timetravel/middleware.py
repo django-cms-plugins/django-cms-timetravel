34ef29e33b8be703121acacc03ba62e2fb5690fb
Janneke Janssen <j.janssen@lukkien.com>
2013-03-01 12:07:37 +0100

    Middleware fix

--- cms_timetravel/middleware.py
+++ cms_timetravel/middleware.py
@@ -3,6 +3,6 @@
 class TimetravelMiddleware(object):
 
     def process_request(self, request):
-        from cms.utils import timezone
         from cms_timetravel.utils import set_timetravel_date
-        set_timetravel_date(request.session.get('timetravel_date', timezone.now()))
+        if request.session.get('timetravel_date'):
+            set_timetravel_date(request.session['timetravel_date'])
--- cms_timetravel/utils.py
+++ cms_timetravel/utils.py
@@ -1,10 +1,12 @@
 import logging
 from cms.utils import timezone
+#from cms.utils.permissions import _thread_locals
 
 try:
     from threading import local
 except ImportError:
     from django.utils._threading_local import local
+
 
 _thread_locals = local()
 
